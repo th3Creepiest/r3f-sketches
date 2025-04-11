3218c9c2501f1355087dae7130846ecb13dcf865
creep <th3creepiest@gmail.com>
2025-04-11 16:44:54 +0200

    spotlight with helper

--- src/pages/Lights.tsx
+++ src/pages/Lights.tsx
@@ -1,10 +1,23 @@
-import { OrbitControls } from "@react-three/drei"
+import { useRef } from "react"
+import { SpotLightHelper } from "three"
+import { OrbitControls, useHelper } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import { useControls } from "leva"
+
+function SpotLightWithHelper() {
+  const light = useRef(null!)
+  const { angle, penumbra } = useControls({
+    angle: { value: Math.PI / 8, min: 0, max: 1 },
+    penumbra: { value: 0.5, min: 0, max: 1, step: 0.1 },
+  })
+  useHelper(light, SpotLightHelper, "orange")
+  return <spotLight ref={light} intensity={10} position={[0, 2, 0]} angle={angle} penumbra={penumbra} />
+}
 
 export default function Example() {
   return (
     <>
-      <section className="h-screen bg-neutral-900">
+      <section className="bg-neutral-900">
         <div>
           Ambient Light
           <Canvas>
@@ -42,6 +55,17 @@ export default function Example() {
           Point Light
           <Canvas>
             <pointLight position={[0, 2, 0]} intensity={10} />
+            <mesh>
+              <boxGeometry args={[2, 2, 2]} />
+              <meshStandardMaterial color="lime" />
+              <OrbitControls enableZoom={false} />
+            </mesh>
+          </Canvas>
+        </div>
+        <div>
+          Spot Light with Helper
+          <Canvas>
+            <SpotLightWithHelper />
             <mesh>
               <boxGeometry args={[2, 2, 2]} />
               <meshStandardMaterial color="lime" />
