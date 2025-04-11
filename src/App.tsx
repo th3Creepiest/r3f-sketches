8d9e715ae0aa08f24f79d1107f1cb5ae35d68cbc
creep <th3creepiest@gmail.com>
2025-04-11 12:53:17 +0200

    navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import Example1 from "./pages/Example1"
-import Example2 from "./pages/Example2"
-import Example3 from "./pages/Example3"
+import { Link } from "react-router"
 
 export default function App() {
   return (
-    <div className="h-screen bg-amber-400 ">
-      <Example1 />
-      <Example2 />
-      <Example3 />
+    <div>
+      <Link to="/example-1">Example 1</Link>
+      <Link to="/example-2">Example 2</Link>
+      <Link to="/example-3">Example 3</Link>
+      <Link to="/primitives">Primitives</Link>
+      <Link to="/transforms">Transforms</Link>
+      <Link to="/first-person-controls">First Person Controls</Link>
+      <Link to="/orbit-controls">Orbit Controls</Link>
     </div>
   )
 }
