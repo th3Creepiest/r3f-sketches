3bbdc142fbcfed61e08ea5b41f4267b5ff89e02f
creep <th3creepiest@gmail.com>
2025-04-11 23:21:27 +0200

    removed a thing about spotlights

--- src/pages/Lights.tsx
+++ src/pages/Lights.tsx
@@ -1,18 +1,5 @@
-import { useRef } from "react"
-import { SpotLightHelper } from "three"
-import { OrbitControls, useHelper } from "@react-three/drei"
+import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
-import { useControls } from "leva"
-
-function SpotLightWithHelper() {
-  const light = useRef(null!)
-  const { angle, penumbra } = useControls({
-    angle: { value: Math.PI / 8, min: 0, max: 1 },
-    penumbra: { value: 0.5, min: 0, max: 1, step: 0.1 },
-  })
-  useHelper(light, SpotLightHelper, "orange")
-  return <spotLight ref={light} intensity={10} position={[0, 2, 0]} angle={angle} penumbra={penumbra} />
-}
 
 export default function Example() {
   return (
@@ -55,17 +42,6 @@ export default function Example() {
           Point Light
           <Canvas>
             <pointLight position={[0, 2, 0]} intensity={10} />
-            <mesh>
-              <boxGeometry args={[2, 2, 2]} />
-              <meshStandardMaterial color="lime" />
-              <OrbitControls enableZoom={false} />
-            </mesh>
-          </Canvas>
-        </div>
-        <div>
-          Spot Light with Helper
-          <Canvas>
-            <SpotLightWithHelper />
             <mesh>
               <boxGeometry args={[2, 2, 2]} />
               <meshStandardMaterial color="lime" />
