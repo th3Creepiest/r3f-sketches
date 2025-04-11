69bbc49242e877609e32eccbd2625739ffab50d6
creep <th3creepiest@gmail.com>
2025-04-11 13:00:12 +0200

    remove leading slashes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router"
 export default function App() {
   return (
     <div>
-      <Link to="/example-1">Example 1</Link>
-      <Link to="/example-2">Example 2</Link>
-      <Link to="/example-3">Example 3</Link>
-      <Link to="/primitives">Primitives</Link>
-      <Link to="/transforms">Transforms</Link>
-      <Link to="/first-person-controls">First Person Controls</Link>
-      <Link to="/orbit-controls">Orbit Controls</Link>
+      <Link to="example-1">Example 1</Link>
+      <Link to="example-2">Example 2</Link>
+      <Link to="example-3">Example 3</Link>
+      <Link to="primitives">Primitives</Link>
+      <Link to="transforms">Transforms</Link>
+      <Link to="first-person-controls">First Person Controls</Link>
+      <Link to="orbit-controls">Orbit Controls</Link>
     </div>
   )
 }
