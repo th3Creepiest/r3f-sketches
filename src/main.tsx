8838d664e29a69521503809713b8715c2ffed3bf
creep <th3creepiest@gmail.com>
2025-04-13 14:41:10 +0200

    reorganize route imports

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router"
 import "./index.css"
 
 import App from "./App.tsx"
-import Example1 from "./pages/Example1"
-import Example2 from "./pages/Example2"
-import Example3 from "./pages/Example3"
-import Primitives from "./pages/Primitives"
-import Transforms from "./pages/Transforms"
-import HelperGizmos from "./pages/HelperGizmos.tsx"
-import LevaGui from "./pages/LevaGui.tsx"
 
 import Cameras from "./pages/cameras/Cameras.tsx"
 import FirstPersonControls from "./pages/cameras/FirstPersonControls.tsx"
@@ -29,19 +22,19 @@ import MeshBasicMaterial from "./pages/materials/MeshBasicMaterial.tsx"
 import MeshLambertMaterial from "./pages/materials/MeshLambertMaterial.tsx"
 import MeshPhongMaterial from "./pages/materials/MeshPhongMaterial.tsx"
 
+import Example1 from "./pages/Example1"
+import Example2 from "./pages/Example2"
+import Example3 from "./pages/Example3"
+import Primitives from "./pages/Primitives"
+import Transforms from "./pages/Transforms"
+import HelperGizmos from "./pages/HelperGizmos.tsx"
+import LevaGui from "./pages/LevaGui.tsx"
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-
-        <Route path="example-1" element={<Example1 />} />
-        <Route path="example-2" element={<Example2 />} />
-        <Route path="example-3" element={<Example3 />} />
-        <Route path="primitives" element={<Primitives />} />
-        <Route path="transforms" element={<Transforms />} />
-        <Route path="helper-gizmos" element={<HelperGizmos />} />
-        <Route path="leva-gui" element={<LevaGui />} />
 
         <Route path="cameras" element={<Cameras />} />
         <Route path="first-person-controls" element={<FirstPersonControls />} />
@@ -59,6 +52,14 @@ createRoot(document.getElementById("root")!).render(
         <Route path="mesh-basic-material" element={<MeshBasicMaterial />} />
         <Route path="mesh-lambert-material" element={<MeshLambertMaterial />} />
         <Route path="mesh-phong-material" element={<MeshPhongMaterial />} />
+
+        <Route path="example-1" element={<Example1 />} />
+        <Route path="example-2" element={<Example2 />} />
+        <Route path="example-3" element={<Example3 />} />
+        <Route path="primitives" element={<Primitives />} />
+        <Route path="transforms" element={<Transforms />} />
+        <Route path="helper-gizmos" element={<HelperGizmos />} />
+        <Route path="leva-gui" element={<LevaGui />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
