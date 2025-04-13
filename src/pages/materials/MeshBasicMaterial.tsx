2e3d0ccdaa4034efd2afadae972afdbe54b4afcd
creep <th3creepiest@gmail.com>
2025-04-13 13:55:25 +0200

    basic material

--- src/pages/materials/MeshBasicMaterial.tsx
+++ src/pages/materials/MeshBasicMaterial.tsx
@@ -1,17 +1,26 @@
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
+import { useControls, folder } from "leva"
 
 export default function Example() {
+  const { color, wireframe } = useControls({
+    Basic_Material_Settings: folder({
+      color: { value: "royalblue" },
+      wireframe: { value: false },
+    }),
+  })
+
   return (
-    <Canvas>
-      <mesh>
+    <div className="h-screen">
+      <Canvas>
         <ambientLight />
-        <pointLight position={[5, 5, 5]} />
-        <pointLight position={[-3, -3, 2]} />
-        <sphereGeometry />
-        <meshBasicMaterial color="hotpink" />
-      </mesh>
-      <OrbitControls />
-    </Canvas>
+        <pointLight position={[3, 3, 3]} intensity={100} />
+        <mesh>
+          <sphereGeometry />
+          <meshBasicMaterial color={color} wireframe={wireframe} />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
   )
 }
