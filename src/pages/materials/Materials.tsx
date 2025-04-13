4d437f43cb85682aabb67b9608cfa29d61246ca7
creep <th3creepiest@gmail.com>
2025-04-13 14:15:15 +0200

    enhance 3D scene with additional meshes and controls

--- src/pages/materials/Materials.tsx
+++ src/pages/materials/Materials.tsx
@@ -1,12 +1,25 @@
 import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
 
 export default function Example() {
   return (
-    <Canvas>
-      <mesh>
-        <sphereGeometry />
-        <meshBasicMaterial color="hotpink" />
-      </mesh>
-    </Canvas>
+    <div className="h-screen">
+      <Canvas>
+        <ambientLight />
+        <mesh position={[-1, 0, 0]}>
+          <sphereGeometry />
+          <meshBasicMaterial color="hotpink" />
+        </mesh>
+        <mesh position={[0, 0, 0]}>
+          <sphereGeometry />
+          <meshLambertMaterial color="hotpink" />
+        </mesh>
+        <mesh position={[1, 0, 0]}>
+          <sphereGeometry />
+          <meshPhongMaterial color="hotpink" />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
   )
 }
