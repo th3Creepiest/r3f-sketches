6a8666d1338200b7beb752bb664b84ebc30732f8
creep <th3creepiest@gmail.com>
2025-04-13 20:49:07 +0200

    remove bg-white and add point lights for better visibility

--- src/pages/cameras/FirstPersonControls.tsx
+++ src/pages/cameras/FirstPersonControls.tsx
@@ -3,9 +3,13 @@ import { FirstPersonControls } from "@react-three/drei"
 
 export default function Example() {
   return (
-    <div className="h-screen bg-white">
+    <div className="h-screen">
       <Canvas>
         <ambientLight />
+        <pointLight position={[0, 1, 1]} />
+        <pointLight position={[0, 1, -1]} />
+        <pointLight position={[0.5, 1, 0]} />
+        <pointLight position={[-0.5, 1, 0]} />
         <FirstPersonControls movementSpeed={3} />
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
--- src/pages/cameras/OrbitControls.tsx
+++ src/pages/cameras/OrbitControls.tsx
@@ -3,14 +3,18 @@ import { OrbitControls } from "@react-three/drei"
 
 export default function Example() {
   return (
-    <div className="h-screen bg-white">
+    <div className="h-screen">
       <Canvas>
         <ambientLight />
-        <OrbitControls />
+        <pointLight position={[0, 1, 1]} />
+        <pointLight position={[0, 1, -1]} />
+        <pointLight position={[0.5, 1, 0]} />
+        <pointLight position={[-0.5, 1, 0]} />
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color="hotpink" />
         </mesh>
+        <OrbitControls />
       </Canvas>
     </div>
   )
