d155bebf06eb9830b3dd53ef47e804b6513a4eee
creep <th3creepiest@gmail.com>
2025-04-13 13:36:09 +0200

    phong material

--- src/pages/materials/MeshPhongMaterial.tsx
+++ src/pages/materials/MeshPhongMaterial.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useRef } from "react"
+import { MeshPhongMaterial } from "three"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
+import { useControls, folder } from "leva"
 
 export default function Example() {
+  const material = useRef<MeshPhongMaterial>(null)
+  const { color, shininess, flatShading, wireframe } = useControls({
+    Material_Settings: folder({
+      color: { value: "royalblue" },
+      shininess: { value: 30, min: 0, max: 250 },
+      wireframe: { value: false },
+      flatShading: { value: false },
+    }),
+  })
+
+  useEffect(() => {
+    if (material.current) {
+      material.current.flatShading = flatShading
+      material.current.needsUpdate = true
+    }
+  }, [flatShading])
+
   return (
     <div className="h-screen">
       <Canvas>
+        <ambientLight />
+        <pointLight position={[3, 3, 3]} intensity={100} />
         <mesh>
-          <ambientLight />
-          <pointLight position={[5, 5, 5]} />
-          <pointLight position={[-3, -3, 2]} />
           <sphereGeometry />
-          <meshPhongMaterial color="hotpink" />
+          <meshPhongMaterial
+            ref={material}
+            color={color}
+            shininess={shininess}
+            wireframe={wireframe}
+          />
         </mesh>
         <OrbitControls />
       </Canvas>
