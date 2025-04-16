d06310e9324f429037e597b4f980cbe1751c46e2
creep <th3creepiest@gmail.com>
2025-04-16 13:43:11 +0200

    simplify material controls

--- src/pages/materials/MeshPhysicalMaterial.tsx
+++ src/pages/materials/MeshPhysicalMaterial.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useRef } from "react"
 import { MeshPhysicalMaterial } from "three"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
@@ -6,34 +6,27 @@ import { useControls, folder } from "leva"
 
 export default function Example() {
   const material = useRef<MeshPhysicalMaterial>(null)
-  const {
-    color,
-    emissiveColor,
-    roughness,
-    metalness,
-    clearCoat,
-    clearCoatRoughness,
-    wireframe,
-    flatShading,
-  } = useControls({
+
+  const { ...materialControls } = useControls({
     Physical_Material_Settings: folder({
       color: { value: "royalblue" },
-      emissiveColor: { value: "black" },
+      emissive: { value: "black" },
       roughness: { value: 1, min: 0, max: 1 },
       metalness: { value: 0, min: 0, max: 1 },
-      clearCoat: { value: 0, min: 0, max: 1 },
-      clearCoatRoughness: { value: 0, min: 0, max: 1 },
+      clearcoat: { value: 0, min: 0, max: 1 },
+      clearcoatRoughness: { value: 0, min: 0, max: 1 },
       wireframe: { value: false },
-      flatShading: { value: false },
+      flatShading: {
+        value: false,
+        onChange: (v) => {
+          if (material.current) {
+            material.current.flatShading = v
+            material.current.needsUpdate = true
+          }
+        },
+      },
     }),
   })
-
-  useEffect(() => {
-    if (material.current) {
-      material.current.flatShading = flatShading
-      material.current.needsUpdate = true
-    }
-  }, [flatShading])
 
   return (
     <div className="h-screen">
@@ -42,16 +35,7 @@ export default function Example() {
         <pointLight position={[3, 3, 3]} intensity={100} />
         <mesh>
           <sphereGeometry />
-          <meshPhysicalMaterial
-            ref={material}
-            color={color}
-            emissive={emissiveColor}
-            roughness={roughness}
-            metalness={metalness}
-            clearcoat={clearCoat}
-            clearcoatRoughness={clearCoatRoughness}
-            wireframe={wireframe}
-          />
+          <meshPhysicalMaterial ref={material} {...materialControls} />
         </mesh>
         <OrbitControls />
       </Canvas>
