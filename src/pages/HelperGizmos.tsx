3b4191b0c9336b912f95de61d974f548469034cd
creep <th3creepiest@gmail.com>
2025-04-19 17:07:14 +0200

    feat: add Stats and StatsGl components to Example page

--- src/pages/HelperGizmos.tsx
+++ src/pages/HelperGizmos.tsx
@@ -1,25 +1,21 @@
 import { useRef } from "react"
 import { Mesh } from "three"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { OrbitControls, GizmoHelper, GizmoViewcube, GizmoViewport } from "@react-three/drei"
-
-function RotatingCube() {
-  const boxRef = useRef<Mesh>(null!)
-  useFrame(() => ((boxRef.current.rotation.x += 0.01), (boxRef.current.rotation.y += 0.01)))
-  return (
-    <mesh ref={boxRef} position={[1, 1, 1]}>
-      <boxGeometry args={[0.5, 0.5, 0.5]} />
-      <meshStandardMaterial color="lime" />
-      <axesHelper />
-      <gridHelper args={[2, 8, "hotpink", "royalblue"]} />
-    </mesh>
-  )
-}
+import {
+  OrbitControls,
+  GizmoHelper,
+  GizmoViewcube,
+  GizmoViewport,
+  Stats,
+  StatsGl,
+} from "@react-three/drei"
 
 export default function Example() {
   return (
     <div className="h-screen bg-gray-800">
       <Canvas>
+        <Stats showPanel={0} />
+        <StatsGl />
         <mesh>
           <ambientLight intensity={0.5} />
           <pointLight position={[1.5, 1.5, 1]} />
@@ -34,5 +30,20 @@ export default function Example() {
         </mesh>
       </Canvas>
     </div>
+  )
+}
+
+function RotatingCube() {
+  const boxRef = useRef<Mesh>(null!)
+  useFrame(
+    () => ((boxRef.current.rotation.x += 0.01), (boxRef.current.rotation.y += 0.01))
+  )
+  return (
+    <mesh ref={boxRef} position={[1, 1, 1]}>
+      <boxGeometry args={[0.5, 0.5, 0.5]} />
+      <meshStandardMaterial color="lime" />
+      <axesHelper />
+      <gridHelper args={[2, 8, "hotpink", "royalblue"]} />
+    </mesh>
   )
 }
