a5ceb338ab5c6bf0b8b7f76bc671ffac837a5e89
creep <th3creepiest@gmail.com>
2025-04-19 16:37:52 +0200

    refactor(animations): restructure Example.tsx

--- src/pages/animations/Example.tsx
+++ src/pages/animations/Example.tsx
@@ -1,26 +1,84 @@
-import { Canvas, useFrame } from "@react-three/fiber"
-import { useRef } from "react"
 import { Mesh } from "three"
+import { useRef } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
 
-function Box() {
+export default function Example() {
+  return (
+    <div className="h-screen">
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 10, 10]} intensity={0.5} />
+        <group position={[-3, 0, 0]}>
+          <AnimatedBox1 />
+          <AnimatedBox2 />
+          <AnimatedBox3 />
+          <AnimatedBox4 />
+        </group>
+      </Canvas>
+    </div>
+  )
+}
+
+function AnimatedBox1() {
   const boxRef = useRef<Mesh>(null!)
-  useFrame(() => ((boxRef.current.rotation.x += 0.01), (boxRef.current.rotation.y += 0.01)))
+
+  useFrame(() => {
+    boxRef.current.rotation.x += 0.01
+    boxRef.current.rotation.y += 0.01
+  })
+
   return (
     <mesh ref={boxRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="royalblue" />
+    </mesh>
+  )
+}
+
+function AnimatedBox2() {
+  const boxRef = useRef<Mesh>(null!)
+
+  useFrame(({ clock }) => {
+    boxRef.current.rotation.x = clock.elapsedTime
+    boxRef.current.rotation.y = clock.elapsedTime
+  })
+
+  return (
+    <mesh ref={boxRef} position-x={2}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color="hotpink" />
     </mesh>
   )
 }
 
-export default function Example() {
+function AnimatedBox3() {
+  const boxRef = useRef<Mesh>(null!)
+
+  useFrame(({ clock }) => {
+    boxRef.current.rotation.x = Math.sin(clock.elapsedTime)
+    boxRef.current.rotation.y = Math.sin(clock.elapsedTime)
+  })
+
   return (
-    <div className="h-screen bg-black">
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[1, 1, 1]} />
-        <Box />
-      </Canvas>
-    </div>
+    <mesh ref={boxRef} position-x={4}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  )
+}
+
+function AnimatedBox4() {
+  const boxRef = useRef<Mesh>(null!)
+
+  useFrame(({ clock }) => {
+    boxRef.current.rotation.x = Math.cos(clock.elapsedTime)
+    boxRef.current.rotation.y = Math.cos(clock.elapsedTime)
+  })
+
+  return (
+    <mesh ref={boxRef} position-x={6}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
   )
 }
